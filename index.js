e9ef4918f4c06a6d4e1f762cec074d3702bcddb1
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-27 23:29:09 +0600

    This is the products

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const { query } = require('express');
+var cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -57,9 +57,8 @@ async function run() {
                 const bookedSlots = optionBooked.map(book => book.slot);
                 const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
                 option.slots = remainingSlots;
-            })
-
-            res.send(options)
+            });
+            res.send(options);
         });
 
         app.get('/v2/appointmentOptions', async (req, res) => {
@@ -138,7 +137,7 @@ async function run() {
 
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
-            res.send(bookings)
+            res.send(bookings);
 
         });
 
@@ -149,8 +148,7 @@ async function run() {
                 appointmentDate: booking.appointmentDate,
                 email: booking.email,
                 treatment: booking.treatment
-            }
-
+            };
             const alreadyBooked = await bookingsCollection.find(query).toArray();
 
             if (alreadyBooked.length) {
@@ -163,7 +161,6 @@ async function run() {
 
         });
 
-
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
@@ -171,9 +168,8 @@ async function run() {
             if (user) {
                 const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
                 return res.send({ accessToken: token })
-            }
-
-            res.status(403).send({ accessToken: 'Unauthorized' })
+            };
+            res.status(403).send({ accessToken: 'Unauthorized' });
         });
 
 
@@ -188,14 +184,10 @@ async function run() {
             res.send(users);
         });
 
-
-
         app.post('/users', async (req, res) => {
             const user = req.body;
-            // console.log(user);
             const result = await usersCollection.insertOne(user);
             res.send(result);
-
         });
 
 
@@ -206,7 +198,7 @@ async function run() {
 
         app.put('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: ObjectId(id) }
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
@@ -215,9 +207,7 @@ async function run() {
             }
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
-
         });
-
 
         /* 
         ---------------------------------------------
@@ -237,15 +227,13 @@ async function run() {
             const result = await doctorsCollection.find(query).toArray();
             res.send(result)
         });
+
         app.delete('/doctors/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await doctorsCollection.deleteOne(filter);
             res.send(result);
         });
-
-
-
 
         /* --------------------------------
           Payment System integrate 
@@ -261,29 +249,14 @@ async function run() {
             }
             const result = await appointmentOptionsCollection.updateMany(filter, updatedDoc, options);
             res.send(result);
-        })
-
-
-
-
-
+        });
     }
-
-
-
-
 
     finally {
         //   await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
-
-
 
 
 app.get('/', (req, res) => {
